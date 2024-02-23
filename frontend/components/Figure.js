6c7d04e6cbb9a99a48ebbdacbe64e6c7dd77c40b
Yemmieeyc <adewole_aina@yahoo.com>
2024-02-23 13:15:53 -0800

    module 4 done

--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,9 +1,9 @@
-import React from "react";
-export default function Figure({imageUrl, caption}) {
-  return (
-    <figure>
-      <img src={imageUrl}/>
-      <figcaption>Awesome pic taken on {caption}</figcaption>
-    </figure>
-  )
-}
+// import React from "react";
+// export default function Figure({imageUrl, caption}) {
+//   return (
+//     <figure>
+//       <img src={imageUrl}/>
+//       <figcaption>Awesome pic taken on {caption}</figcaption>
+//     </figure>
+//   )
+// }
